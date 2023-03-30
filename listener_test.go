2892646bd542190611b7f0c7cbe64900c1d260d5
Andres Rios <rianby64@gmail.com>
2023-03-30 16:43:41 +0300

    Test recoverable rrror during message fetching

--- listener_test.go
+++ listener_test.go
@@ -46,14 +46,15 @@ func (m *MockKafkaReader) Close() error {
 	return err
 }
 
-func TestListen(t *testing.T) {
-	t.Parallel()
+// func TestListen(t *testing.T) {
+// 	t.Parallel()
 
-	t.Run("Successful message processing", TestSuccessfulMessageProcessing)
-	t.Run("Error unrecoverable during message fetching", TestUnrecoverableErrorDuringMessageFetching)
-	// t.Run("Message processing timeout", testMessageProcessingTimeout)
-	// t.Run("Context cancellation", testContextCancellation)
-}
+// 	t.Run("Successful message processing", TestSuccessfulMessageProcessing)
+// 	t.Run("Error unrecoverable during message fetching", TestUnrecoverableErrorDuringMessageFetching)
+// 	t.Run("Error recoverable during message fetching", TestRecoverableErrorDuringMessageFetching)
+// 	// t.Run("Message processing timeout", testMessageProcessingTimeout)
+// 	// t.Run("Context cancellation", testContextCancellation)
+// }
 
 func TestSuccessfulMessageProcessing(t *testing.T) {
 	t.Parallel()
@@ -150,4 +151,58 @@ func TestUnrecoverableErrorDuringMessageFetching(t *testing.T) {
 
 	assert.ErrorIs(t, err, errorForFetchMessage)
 	assert.False(t, msgProcessed)
+}
+
+func TestRecoverableErrorDuringMessageFetching(t *testing.T) {
+	t.Parallel()
+
+	msg := []byte("test message")
+	errorForFetchMessage := kafka.NetworkException
+
+	mockReader := new(MockKafkaReader)
+	mockReader.On("FetchMessage", mock.Anything).Return(kafka.Message{Value: msg}, &errorForFetchMessage)
+	mockReader.On("CommitMessages", mock.Anything, []kafka.Message{{Value: msg}}).Return(nil)
+	mockReader.On("Close").Return(nil)
+
+	// Set up a simple logger to collect logs during testing
+	log := log.NewLogger()
+
+	reconnections := 0
+
+	// Create a test context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	opts := listener.NewOptions().
+		WithProcessingTimeout(1 * time.Second).
+		WithReconnectInterval(1 * time.Second).
+		WithReaderFactory(func() listener.Reader {
+			reconnections++
+
+			return mockReader
+		})
+
+	listener := listener.NewListener(log, opts)
+
+	msgProcessed := false
+
+	go func() {
+		// I want to be sure the msgChan and errChan behave as expected
+		msgChan, errChan := listener.MessageAndErrorChannels()
+		assert.Equal(t, msg, <-msgChan)
+		errChan <- nil
+
+		// If we are here it's because we have the expected message
+		msgProcessed = true
+
+		// So, shutdown
+		err := listener.Shutdown(ctx)
+		assert.NoError(t, err)
+	}()
+
+	err := listener.Listen(ctx)
+
+	assert.ErrorIs(t, err, context.DeadlineExceeded)
+	assert.False(t, msgProcessed)
+	assert.GreaterOrEqual(t, reconnections, 2)
 }
