54031f5296e3aedb1560922667f51825e4d5ce5e
Andres Rios <rianby64@gmail.com>
2023-08-24 01:07:12 +0300

    Add more information to this case

--- publisher-api.go
+++ publisher-api.go
@@ -2,8 +2,6 @@ package kafko
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
@@ -11,19 +9,57 @@ import (
 )
 
 func (publisher *Publisher) Publish(ctx context.Context, payload []byte) error {
-	now := time.Now()
-	writer := publisher.writer
+	/* I want to expose here a simple strategy in order to deal with
+	   some errors we're experiencing in the network...
 
+		if publisher.stateError {
+			launch a recovery strategy
+				- write in the disk
+				- send to an available kafka topic
+				- write to mongoDB
+				- ...
+				- log this thing
+				- who cares!!!
+
+			return
+		}
+
+		writer := publisher.writer
+	    for {
+			if err := writer.WriteMessage(); err != nil {
+				ATTENTION:  the first failer MUST block other attempts,
+				            therefore other running attempts must fall into
+							the recovery strategy
+
+				set publisher.stateError to error,
+				so other attempts to write will be redirected to
+				the recovery strategy
+
+				wait some timeout then
+				if err := writer.Close(); err != nil {
+					just log the error, and we know that here we've lost
+					the current batch of messages, so, bad luck.
+				}
+
+				publisher.writer = opts.writerFactory() // build a new connection
+				continue // in order to repeat this attempt, at least 3 times
+			}
+
+			// if you are here
+			set publisher.stateError to NO-error,
+			so other attempts will take place as expected
+
+			return // so break the loop
+		}
+	*/
+
+	writer := publisher.writer
 	if err := writer.WriteMessages(ctx, kafka.Message{
 		Key:   []byte(uuid.New().String()),
 		Value: payload,
 	}); err != nil {
 		return errors.Wrapf(err, "cannot write message to Kafka")
 	}
-
-	defer func() {
-		fmt.Print("\033[G\033[K", time.Since(now), "\n", "\033[A")
-	}()
 
 	return nil
 }
