90f7c1bc8519c148513a6ae77eecbbc0b1d9f21b
Andres Rios <rianby64@gmail.com>
2023-09-25 19:18:06 +0300

    Start the simplest implementation of the listener

--- listener_test.go
+++ listener_test.go
@@ -1,7 +1,45 @@
 package kafko_test
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/m3co/kafko"
+	"github.com/m3co/kafko/log"
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockListenerMsgHangler_OK struct { //nolint:revive,stylecheck
+}
+
+func (MockListenerMsgHangler_OK) Handle(context.Context, *kafka.Message) error {
+	return nil
+}
 
 func Test_Listener_OK(t *testing.T) {
+	t.Parallel()
 
+	waitUntilTheEnd := make(chan struct{}, 1)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	actualLog := log.NewMockLogger()
+
+	listener := kafko.NewListener(actualLog, kafko.NewOptionsListener().WithHandler(new(MockListenerMsgHangler_OK)))
+
+	go func(t *testing.T) {
+		t.Helper()
+
+		actualErr := listener.Shutdown(ctx)
+		assert.Nil(t, actualErr)
+
+		waitUntilTheEnd <- struct{}{}
+	}(t)
+
+	actualErr := listener.Listen(ctx)
+	assert.Nil(t, actualErr)
+
+	<-waitUntilTheEnd
 }
