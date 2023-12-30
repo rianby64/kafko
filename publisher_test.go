e49345197d67ebc8984423677d00c81334eb78e8
Andres Rios <rianby64@gmail.com>
2023-12-30 15:26:45 +0200

    checkpoint

--- publisher_test.go
+++ publisher_test.go
@@ -3,6 +3,7 @@ package kafko_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"kafko"
 	"kafko/log"
@@ -206,8 +207,7 @@ func Test_Publisher_single_message_WriteMessages_err_handle_dropped_msg_err(t *t
 		Value: mockPayload,
 	}
 
-	ctx := context.Background()
-	ctrl := gomock.NewController(t)
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
 
 	defer ctrl.Finish()
 
@@ -246,4 +246,83 @@ func Test_Publisher_single_message_WriteMessages_err_handle_dropped_msg_err(t *t
 	shutdownErr := publisher.Shutdown(ctx)
 
 	assert.Nil(t, shutdownErr)
+}
+
+func Test_Publisher_call_tooSlow_WriteMessages_but_Shutdown_cancels_WriteMessage_OK(t *testing.T) {
+	t.Parallel()
+
+	mockPayload := []byte("mocked message")
+	mockMessage := kafka.Message{
+		Value: mockPayload,
+	}
+
+	inform := make(chan struct{}, 1)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	ctrl := gomock.NewController(t)
+
+	defer ctrl.Finish()
+
+	ctxMatcher := newContextMatcherForPublisher(ctx)
+	mockWriter := mocks.NewMockWriter(ctrl)
+	mockHandlerDroppedMsg := mocks.NewMockMsgHandler(ctrl)
+
+	gomock.InOrder(
+		mockWriter.EXPECT().
+			WriteMessages(ctxMatcher, mockMessage).
+			DoAndReturn(func(ctx context.Context, msg kafka.Message) error {
+				go func() {
+					inform <- struct{}{}
+				}()
+
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-time.After(time.Second):
+					t.Fatal("unexpected execution")
+
+					return nil
+				}
+			}),
+
+		mockHandlerDroppedMsg.EXPECT().
+			Handle(ctxMatcher, &mockMessage).
+			Return(errRandomForHandlerDroppedMsg),
+
+		mockWriter.EXPECT().
+			Close().
+			Return(nil),
+	)
+
+	actualLog := log.NewMockLogger()
+	opts := kafko.NewOptionsPublisher().
+		WithWriterFactory(func() kafko.Writer {
+			return mockWriter
+		}).
+		WithHandlerDropped(mockHandlerDroppedMsg)
+
+	publisher := kafko.NewPublisher(actualLog, opts)
+
+	go func() {
+		<-inform
+
+		cancel()
+
+		time.Sleep(time.Millisecond) // temporary hack
+
+		shutdownErr := publisher.Shutdown(ctx)
+
+		assert.Nil(t, shutdownErr)
+		close(inform)
+	}()
+
+	publishErr := publisher.Publish(ctx, mockPayload)
+
+	assert.ErrorIs(t, publishErr, errRandomForHandlerDroppedMsg)
+	assert.ErrorIs(t, publishErr, context.Canceled)
+
+	_, ok := <-inform
+	t.Log(ok)
 }
